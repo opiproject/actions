0a7079d19ea297533ea5fb217a8affb06889345f
Boris Glimcher <36732377+glimchb@users.noreply.github.com>
2023-09-24 00:36:29 -0400

    ci: add example main.go
    Signed-off-by: Boris Glimcher <36732377+glimchb@users.noreply.github.com>
    Signed-off-by: Boris Glimcher <Boris.Glimcher@emc.com>

---
+++ cmd/main.go
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (c) 2022-2023 Dell Inc, or its subsidiaries.
+
+// Package main is just an example
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("vim-go")
+}
